5fa0e60efdac27d112090e906553452c3822d151
2E4O8T <benoit.froidevan@axa.fr>
2023-04-14 11:13:34 +0200

    premier commit

---
+++ Data/Migrations/20230414085749_numberone.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EVSec.Data.Migrations
+{
+    public partial class numberone : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
